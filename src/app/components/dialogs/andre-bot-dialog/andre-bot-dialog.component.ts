95d64c04dc4f77d7b7f59db83217d2613e4e1675
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2024-05-03 01:40:13 -0300

    Added André Bot

--- src/app/components/dialogs/andre-bot-dialog/andre-bot-dialog.component.ts
+++ src/app/components/dialogs/andre-bot-dialog/andre-bot-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-andre-bot-dialog',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './andre-bot-dialog.component.scss'
 })
 export class AndreBotDialogComponent {
+
+	@ViewChild("content")
+	content: ElementRef;
+
+	readonly QuestionOptionTypes = QuestionOptionTypes;
+	readonly OptionsTypes = OptionsTypes;
+
+	isAnswering: boolean = false;
+
+	optionType: OptionsTypes = OptionsTypes.Default;
 
 	messages: Message[] = [
 		{
@@ -21,9 +31,182 @@ export class AndreBotDialogComponent {
 			answer: true,
 		},
 	];
+
+	// TODO code reuse
+	hello() {
+		this.isAnswering = true;
+		this.messages.push(
+			{
+				message: 'Just saying hello!',
+				answer: false
+			}
+		);
+
+		let messagesToAdd: Message[] = [
+			{
+				message: "Hi!",
+				answer: true,
+			},
+			{
+				message: "Thanks for passing by",
+				answer: true,
+			},
+			{
+				message: "Hope you're having fun 😉",
+				answer: true,
+			},
+			{
+				message: "Can I do anything else for you?",
+				answer: true,
+			},
+		];
+
+		for (let i = 0; i < messagesToAdd.length; ++i) {
+			setTimeout(() => {
+				this.messages.push(messagesToAdd[i]);
+				setTimeout(() => {
+					this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+				});
+			}, 1000 * (i + 1));
+		}
+
+		setTimeout(() => {
+			this.isAnswering = false;
+			setTimeout(() => {
+				this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+			});
+		}, 1000 * messagesToAdd.length);
+	}
+
+	hire() {
+		this.isAnswering = true;
+		this.messages.push(
+			{
+				message: "We'd like to hire you",
+				answer: false
+			}
+		);
+
+		let messagesToAdd: Message[] = [
+			{
+				message: "That's awesome!",
+				answer: true,
+			},
+			{
+				message: "Send me a message and let's chat further!",
+				answer: true,
+			},
+			{
+				message: "You can call me at andrelucax@gmail.com",
+				answer: true,
+			},
+		];
+
+		for (let i = 0; i < messagesToAdd.length; ++i) {
+			setTimeout(() => {
+				this.messages.push(messagesToAdd[i]);
+				setTimeout(() => {
+					this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+				});
+			}, 1000 * (i + 1));
+		}
+
+		setTimeout(() => {
+			this.optionType = OptionsTypes.Contact;
+			this.isAnswering = false;
+			setTimeout(() => {
+				this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+			});
+		}, 1000 * messagesToAdd.length);
+	}
+
+	message() {
+		this.isAnswering = true;
+		this.messages.push(
+			{
+				message: "Send a message",
+				answer: false
+			}
+		);
+
+		let messagesToAdd: Message[] = [
+			{
+				message: "I'm sending you to your mail",
+				answer: true,
+			},
+			{
+				message: "Can I do anything else for you?",
+				answer: true,
+			},
+		];
+
+		window.open('mailto:andrelucax@gmail.com?subject=Work%20enquiry');
+		for (let i = 0; i < messagesToAdd.length; ++i) {
+			setTimeout(() => {
+				this.messages.push(messagesToAdd[i]);
+				setTimeout(() => {
+					this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+				});
+			}, 1000 * (i + 1));
+		}
+
+		setTimeout(() => {
+			this.optionType = OptionsTypes.Default;
+			this.isAnswering = false;
+			setTimeout(() => {
+				this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+			});
+		}, 1000 * messagesToAdd.length);
+	}
+
+	options() {
+		this.isAnswering = true;
+		this.messages.push(
+			{
+				message: "See other options",
+				answer: false
+			}
+		);
+
+		let messagesToAdd: Message[] = [
+			{
+				message: "Ok, here you go",
+				answer: true,
+			},
+		];
+
+		for (let i = 0; i < messagesToAdd.length; ++i) {
+			setTimeout(() => {
+				this.messages.push(messagesToAdd[i]);
+				setTimeout(() => {
+					this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+				});
+			}, 1000 * (i + 1));
+		}
+
+		setTimeout(() => {
+			this.optionType = OptionsTypes.Default;
+			this.isAnswering = false;
+			setTimeout(() => {
+				this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+			});
+		}, 1000 * messagesToAdd.length);
+	}
 }
 
 export interface Message {
 	message: string;
 	answer: boolean;
+}
+
+export enum QuestionOptionTypes {
+	Hello = "Hello",
+	Hire = "Hire",
+	Message = "Message",
+	Options = "Options",
+}
+
+export enum OptionsTypes {
+	Default = "Default",
+	Contact = "Contact",
 }
