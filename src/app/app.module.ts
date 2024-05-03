01d4b273cd04fabd4b869b5d9e39cddd7b4a3a91
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2024-05-03 01:40:10 -0300

    Added bootstrap, home, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { LayoutComponent } from './components/layout/layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		LayoutComponent,
+  HeaderComponent,
+  HomeComponent,
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatButtonModule,
 	],
 	providers: [
 		provideAnimationsAsync()
