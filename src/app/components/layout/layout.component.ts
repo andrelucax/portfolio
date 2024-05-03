4d130994247f721f7fd658784ec3979f58380e84
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2024-05-03 01:40:10 -0300

    Added logo animation

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
 	selector: 'app-layout',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class LayoutComponent {
 
+	@ViewChild("sideNavContent") sideNavContent: MatSidenavContent;
+
+	animateLogo: boolean = true;
+
+	ngAfterViewInit(): void {
+		this.sideNavContent.getElementRef().nativeElement.addEventListener('scroll', this.checkLogoAnimation);
+	}
+
+	ngOnDestroy(): void {
+		this.sideNavContent.getElementRef().nativeElement.removeEventListener('scroll', this.checkLogoAnimation);
+	}
+
+	checkLogoAnimation = (_: any) => {
+		const scrollY = this.sideNavContent.getElementRef().nativeElement.scrollTop || document.documentElement.scrollTop;
+		const threshold = 0;
+
+		if (scrollY > threshold) {
+			this.animateLogo = false;
+		} else {
+			this.animateLogo = true;
+		}
+	}
 }
