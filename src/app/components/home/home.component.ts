423ef2f435df44ae59b12e210ce36e12024ba36f
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2024-05-03 01:40:12 -0300

    Refactored social midia icons

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,18 +6,4 @@ import { Component } from '@angular/core';
 	styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-	navigateToSocialMidia(socialMidia: 'github' | 'linkedin' | 'instagram') {
-		switch (socialMidia) {
-			case 'github':
-				window.open("https://github.com/andrelucax/", "_blank");
-				break;
-			case 'instagram':
-				window.open("https://www.linkedin.com/in/andrelucax/", "_blank");
-				break;
-			case 'linkedin':
-				window.open("https://www.instagram.com/andre.lucax/", "_blank");
-				break;
-		}
-	}
 }
